6ecc794cece6f7545ab8629e50d92a7fb42f7c29
Alessandro Pessi <issepela@gmail.com>
2025-12-10 18:29:59 +0100

    update logo and titles

--- backend/settings.py
+++ backend/settings.py
@@ -41,11 +41,11 @@ class _UiSettings(BaseSettings):
         env_ignore_empty=True
     )
 
-    title: str = "Contoso"
+    title: str = "Nice"
     logo: Optional[str] = None
     chat_logo: Optional[str] = None
-    chat_title: str = "Start chatting"
-    chat_description: str = "This chatbot is configured to answer your questions"
+    chat_title: str = "Smart Home Assistant"
+    chat_description: str = "This chatbot is configured to answer your questions about Nice SmartHome products and solutions."
     favicon: str = "/favicon.ico"
     show_share_button: bool = True
     show_chat_history_button: bool = True
